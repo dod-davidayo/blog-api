53e03a42b987750e063cf0e150219adb42659e0d
Durojaiye David <daviddurojaiye683@gmaill.com>
2026-05-10 18:50:20 +0100

    done with create post

--- models/post.py
+++ models/post.py
@@ -10,3 +10,13 @@ class Post(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "contrnt": self.content,
+            "user_id": self.user_id,
+            "created_at": self.created_at.isoformat(),
+            "updated_at": self.updated_at.isoformat()
+        }
